1a4d782492170ee2d9cab17c90577f1f2a044c81
sungmin <sungmin7668@naver.com>
2026-07-24 23:23:49 +0900

    feat: Endpoint

---
+++ src/main/java/hello/hello_NewsBot/controller/HealthController.java
@@ -0,0 +1,13 @@
+package hello.hello_NewsBot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
