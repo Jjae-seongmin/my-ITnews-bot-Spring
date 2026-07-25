19a41b1aa2e84c7e6df700e47db95435cfebd1de
sungmin <sungmin7668@naver.com>
2026-07-25 13:29:35 +0900

    feat: manual Schedular Trigger

--- src/main/java/hello/hello_NewsBot/controller/HealthController.java
+++ src/main/java/hello/hello_NewsBot/controller/HealthController.java
@@ -1,13 +1,23 @@
 package hello.hello_NewsBot.controller;
 
+import hello.hello_NewsBot.service.NewsDeliveryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthController {
+
+    private final NewsDeliveryService newsDeliveryService;
 
     @GetMapping("/")
     public String health() {
         return "OK";
+    }
+
+    @GetMapping("/trigger")
+    public void triggerTest() {
+        newsDeliveryService.deliverDailyNews();
     }
 }
